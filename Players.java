cb9cb629135cd6ca1e4f47e07844c5d1453e5e42
andrewwilliamp <84424039+andrewwilliamp@users.noreply.github.com>
2023-09-07 21:29:05 -0600

    Add files via upload

---
+++ Players.java
@@ -0,0 +1,23 @@
+
+public class Players extends Person {
+
+    private double money;
+
+    public void setMoney(double moneyAmnt) {
+        money = moneyAmnt;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public Players() {
+        money = 1000;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getFirstName() + " " + getLastName() + " Money: $" + money;
+    }
+
+}
