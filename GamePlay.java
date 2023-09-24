e99391f715515c96eb2fd97cc497a2dfc40368c2
andrewwilliamp <84424039+andrewwilliamp@users.noreply.github.com>
2023-09-24 14:31:07 -0600

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 
 public class GamePlay {
-    private static Players[] currentPlayers = new Players[3]; // Declare and initialize the array
+    private static Players[] currentPlayers = new Players[3];   // Declare and initialize the array
+    private static Phrases phrases = new Phrases();             // Create an instance of Phrases
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
@@ -35,29 +36,46 @@ public class GamePlay {
             currentPlayers[i] = player;
         }
 
-        Hosts host = new Hosts();       // Create an instance of Hosts
-        Turn turn = new Turn();         // Create an instance of Turn
-        host.randomizeNum();            // Generate a new random number for the host
+        System.out.println("Please enter the host's name: ");
+        String hostName = scnr.next();
 
+        Hosts host = new Hosts(hostName);       // Create an instance of Hosts
+        host.enterPhrase();
+
+        Turn turn = new Turn();                 // Create an instance of Turn
+
+        int currentPlayerIndex = 0;             // Index to keep track of the current player
         boolean continuePlaying = true;
-        int currentPlayerIndex = 0;     // Index to keep track of the current player
+        boolean gameWon = false;
 
         while (continuePlaying) {
-            host.randomizeNum();        // Generate a new random number for the host
+            System.out.println(host.getFirstName() + " says, \"the phrase to guess is: " +
+                    phrases.getPlayingPhrase() + "\"");
+            Players currentPlayer = currentPlayers[currentPlayerIndex];
+            System.out.println("It is now the following player's turn:");
+            System.out.println(currentPlayers[currentPlayerIndex]);
 
-            boolean guessedCorrectly = false;
-
-            while(!guessedCorrectly) {
-                Players currentPlayer = currentPlayers[currentPlayerIndex];
-                guessedCorrectly = turn.takeTurn(currentPlayer, host);
+            boolean guessedCorrectly = turn.takeTurn(currentPlayer, host, phrases); // Update guessedCorrectly
+            if (guessedCorrectly) {
                 currentPlayerIndex = (currentPlayerIndex + 1) % 3;  // Move to the next player in a circular manner
             }
 
-            System.out.println("Do you want to continue playing? (Y/N)");
-            String playAgainInput = scnr.next();
+            // Check if the phrase is solved after each guess
+            if (!phrases.getPlayingPhrase().contains("_")) {
+                gameWon = true;
+            }
 
-            if (!(playAgainInput.equalsIgnoreCase("Y"))) {
-                continuePlaying = false;
+            if (gameWon) {
+                System.out.println("You solved the puzzle and won the game!");
+                System.out.println("Play another game? (Y/N)");
+                String playAgainInput = scnr.next();
+                if (!(playAgainInput.equalsIgnoreCase("Y"))) {
+                    continuePlaying = false;
+                } else {
+                    host.resetGamePhrase();             // Clear game phrase so host can enter another
+                    host.enterPhrase();
+                    gameWon = false; // Reset the gameWon flag for the new game
+                }
             }
         }
         scnr.close();
