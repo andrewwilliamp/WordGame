de50b6ab512571ac8ed15ad0eed2483d1fa2ad8d
andrewwilliamp <84424039+andrewwilliamp@users.noreply.github.com>
2023-09-24 14:31:30 -0600

    Update Hosts.java

--- Hosts.java
+++ Hosts.java
@@ -1,10 +1,25 @@
-
+import java.util.Scanner;
 
 public class Hosts extends Person {
 
-    public int randomizeNum() {
-        Numbers.generateNumber();       // Call the static method from Numbers class to generate a random number
-        return Numbers.getNum();
+
+    public Hosts(String firstName) {
+        setFirstName(firstName);
+    }
+
+    public void enterPhrase() {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println(getFirstName() + ", enter a phrase for the players to guess: ");
+        String phrase = scnr.nextLine();
+        Phrases.setGamePhrase(phrase);
+
+        System.out.println("The phrase has been set. Let the games begin!");
+    }
+
+    // Method to reset the gamePhrase for a new game
+    public void resetGamePhrase() {
+        Phrases.setGamePhrase("");
     }
 
 
